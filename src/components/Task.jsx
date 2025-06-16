5eb619c8279ac21065a21d115c7db00437e42659
Olya Mateshov <olyamateshov@gmail.com>
2025-06-16 14:51:20 -0400

    Wave 03 Initial commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,31 @@
 import PropTypes from 'prop-types';
+
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
+const Task = (props) => {
+  const completeButtonClicked = () => {
+    props.toggleComplete(props.id);
+  };
+
+  const deleteButtonClicked = () => {
+    props.deleteTask(props.id);
+  };
+
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
+
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
-      <button className="tasks__item__remove button">x</button>
+      <button
+        className={`tasks__item__toggle ${buttonClass}`}
+        onClick={completeButtonClicked}
+      >
+        {props.title}
+      </button>
+      <button className="tasks__item__remove button"
+        onClick={deleteButtonClicked}
+      >
+        x
+      </button>
     </li>
   );
 };
@@ -14,6 +34,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
